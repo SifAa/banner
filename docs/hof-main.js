87bce8a765c8ac373dd1ee756169f2ebd36fc124
“Sif <sif_aagaard_svendsen@hotmail.com>
2019-04-25 14:46:39 +0200

    animation

--- docs/hof-main.js
+++ docs/hof-main.js
@@ -6,3 +6,9 @@ element.addEventListener("click", goToPrototype);
  function goToPrototype(){
 	window.open("https://xd.adobe.com/view/c387e8ef-0e56-4eca-4206-1b52744e9646-b5b6/", '_blank'); 
 } 
+
+document.addEventListener("load", setTimeout(reloadBanner, 18000)); //for at den begynner forfra
+
+function reloadBanner() {
+	location.reload();
+}
